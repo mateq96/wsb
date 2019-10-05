242fc315623c64c197fc7f07c01d56a04cd4c376
mateq96 <mateusz.ellwardt@wp.pl>
2019-10-05 12:43:24 +0200

    <project>

---
+++ src/demo/Demo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+/**
+ *
+ * @author mateusz
+ */
+ 
+ 
+ 
+  class e {    int i;    e(int i) {       this.i = i * 2;    } }  
+class Demo extends e {    public static void main(String[] args) {       Demo f = new Demo(2);    }  
+   Demo(int i) {   super(i);       System.out.println(i);    } } 
+
+ 
